cf3915fc8d361c7280a5ef14bc325f9880e4d8fa
vahdet <vahdet.karatas@gmail.com>
2024-07-30 12:42:10 +0200

    added body section

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,19 +1,22 @@
-import Link from 'next/link';
-import { TbGridDots } from 'react-icons/tb';
+import HomeHeader from '@/components/HomeHeader';
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
 
-export default function HomeHeader() {
+export default function Home() {
   return (
-    <header className='flex justify-end p-5 text-sm'>
-        <div className="flex space-x-4 items-center">
-            <Link href={"https://mail.google.com"} className='hover:underline'>
-                Gmail
-            </Link>
-            <Link href={"https://image.google.com"} className='hover:underline'>
-                Images
-            </Link>
-            <TbGridDots className='bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2'/>
-            <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow'>Sign in</button>
-        </div>
-    </header>
-  )
+    <>
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+          alt='Google Logo'
+          width={300}
+          height={100}
+          priority
+          style={{ width: 'auto' }}
+        />
+        <HomeSearch />
+      </div>
+    </>
+  );
 }
